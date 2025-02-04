2ab44485e3734ba8f90e49a5983b06a38ebf0476
Shubham Hajare <shubham.hajare99@gmail.com>
2025-02-04 09:53:59 +0530

    updating bancked

--- farmbazaar_backend/src/main/java/com/farmbazaar/dao/CategoryDao.java
+++
@@ -1,9 +0,0 @@
-package com.farmbazaar.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.farmbazaar.pojos.Category;
-
-public interface CategoryDao extends JpaRepository<Category, Integer> {
-
-}
--- farmbazaar_backend/src/main/java/com/farmbazaar/dao/CustomerDao.java
+++
@@ -1,9 +0,0 @@
-package com.farmbazaar.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.farmbazaar.pojos.Customer;
-
-public interface CustomerDao extends JpaRepository<Customer, Integer> {
-
-}
